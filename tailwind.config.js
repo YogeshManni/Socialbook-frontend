b1dba2174cfd0f8c8ce7039e63ac9f4477cfdbcb
Yogesh Manni <yogeshmanni786@gmail.com>
2024-09-21 01:38:11 -0700

    Add react router and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      dullwhite: "#f1f5f9",
+      sbutton: "#8b5cf6",
+      button: "#6366F1",
+      orgbutton: "#3b82f6",
+      red: "#dc2626",
+    },
+  },
+  plugins: [],
+};
