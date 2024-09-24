7621263574c78d2b41cf38b70f1826f1594fc599
Yogesh Manni <yogeshmanni786@gmail.com>
2024-09-23 19:34:52 -0700

    Setup firebase and upload files to firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSGSNDID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
---
+++ src/lib/upload.js
@@ -0,0 +1,67 @@
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import { storage } from "./firebase";
+
+const upload = async (file, filename) => {
+  // Create the file metadata
+  /** @type {any} */
+  console.log(file, filename);
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  const storageRef = ref(storage, "images/" + filename);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  return new Promise((resolve, reject) => {
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        reject(`Some error occured + ${error.code}`);
+        /* switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+  
+            // ...
+  
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          } */
+      },
+      () => {
+        console.log("uploaded successfully");
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log(downloadURL);
+          resolve(downloadURL);
+        });
+      }
+    );
+  });
+};
+
+export default upload;
