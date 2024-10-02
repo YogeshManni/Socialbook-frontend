ad772595dd6e76423f783d39a4472cc3862eb8ae
Yogesh Manni <yogeshmanni786@gmail.com>
2024-10-02 12:07:30 -0700

    Add apis to get posts and update data to db

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -19,3 +19,23 @@ export const addPost = async (obj: any) => {
     return res.data;
   });
 };
+
+export const getPostsFromDb = async (obj: any) => {
+  return await axios.post(`${baseURL}/posts/`, obj).then((res) => {
+    return res.data;
+  });
+};
+
+export const updatePostLikesInDb = async (data: any) => {
+  return await axios.post(`${baseURL}/posts/updateLikes`, data).then((res) => {
+    return res.data;
+  });
+};
+
+export const addCommentTodb = async (data: any) => {
+  return await axios
+    .post(`${baseURL}/event/comments/addComments`, data)
+    .then((res) => {
+      return res.data;
+    });
+};
