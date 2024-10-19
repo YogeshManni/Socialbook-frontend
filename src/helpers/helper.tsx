789d0479c424f65c2a3685215f2b7ecd3dba01be
Yogesh Manni <yogeshmanni786@gmail.com>
2024-10-19 03:33:48 -0700

    Set Authorization token in session storage

--- src/helpers/helper.tsx
+++ src/helpers/helper.tsx
@@ -1,19 +1,27 @@
 import { Skeleton } from "antd";
-import { logoutUser } from "../services/api";
+
+import axios from "axios";
 
 export const getUser = () => {
   const user = sessionStorage.getItem("user");
   return user ? JSON.parse(user) : null;
 };
 
-export const setUser = (data: any) => {
-  sessionStorage.setItem("user", JSON.stringify(data));
+export const setUser = (res: any) => {
+  sessionStorage.setItem("token", JSON.stringify(res.token));
+  sessionStorage.setItem("user", JSON.stringify(res.data));
+};
+
+export const setAuthHeader = () => {
+  const token = sessionStorage.getItem("token");
+  axios.defaults.headers.common["Authorization"] = `Bearer ${
+    token ? JSON.parse(token) : null
+  }`;
 };
 
 export const logout = async () => {
   sessionStorage.removeItem("user");
-  const res = await logoutUser({});
-
+  sessionStorage.removeItem("token");
   window.location.href = "/";
 };
 
