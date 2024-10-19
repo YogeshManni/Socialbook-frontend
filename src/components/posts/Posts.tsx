84799e284b376d8af5286e85aa389fe307f86088
Yogesh Manni <yogeshmanni786@gmail.com>
2024-10-19 03:31:18 -0700

    Show loading state until the data is fetched from server

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -11,7 +11,7 @@ import {
   ShareAltOutlined,
   SmileOutlined,
 } from "@ant-design/icons";
-import { Avatar, Badge, Button, Modal } from "antd";
+import { Avatar, Badge, Button, Modal, Skeleton } from "antd";
 import {
   addCommentTodb,
   getPostsFromDb,
@@ -24,7 +24,7 @@ import { Image } from "antd";
 import { getUser } from "../../helpers/helper";
 
 const Posts = () => {
-  const [posts, setPosts]: any = useState([]);
+  const [posts, setPosts]: any = useState();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [postId, setPostId] = useState(0);
   const [comment, setComment] = useState("");
@@ -99,147 +99,151 @@ const Posts = () => {
   }
   return (
     <section className="flex flex-col  items-center">
-      {posts.map((post: any, ind: number) => (
-        <div key={ind}>
-          <div
-            key={ind}
-            className="relative card space-y-4 max-w-[430px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] p-4 rounded-[20px]"
-          >
-            {/* Heading */}
-            <div className="flex justify-between items-center">
-              <div className="flex gap-3 items-center ">
-                <div className="w-8 h-6  cursor-pointer">
-                  <Avatar
-                    size={30}
+      {!posts ? (
+        <Skeleton active />
+      ) : (
+        posts.map((post: any, ind: number) => (
+          <div key={ind}>
+            <div
+              key={ind}
+              className="relative card space-y-4 max-w-[430px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] p-4 rounded-[20px]"
+            >
+              {/* Heading */}
+              <div className="flex justify-between items-center">
+                <div className="flex gap-3 items-center ">
+                  <div className="w-8 h-6  cursor-pointer">
+                    <Avatar
+                      size={30}
+                      src={
+                        post.profilepic
+                          ? post.profilepic
+                          : `https://xsgames.co/randomusers/avatar.php?g=pixel&key=${ind}`
+                      }
+                    />
+                  </div>
+                  <h2 className="font-semibold">{post.username}</h2>
+                </div>
+                {/* <DotsHorizontalIcon className="w-5 h-5 cursor-pointer" /> */}
+              </div>
+
+              {/* Posted Image */}
+
+              {!(post.type === "video") ? (
+                <div className="relative  aspect-square overflow-hidden z-20">
+                  {/* If the image src doesn't contaain https:// i.e if its not from firebase or null then show notFoundImg */}
+                  <Image
                     src={
-                      post.profilepic
-                        ? post.profilepic
-                        : `https://xsgames.co/randomusers/avatar.php?g=pixel&key=${ind}`
+                      post?.img.includes("https://") ? post.img : notFoundImage
                     }
+                    alt={post.username}
                   />
                 </div>
-                <h2 className="font-semibold">{post.username}</h2>
-              </div>
-              {/* <DotsHorizontalIcon className="w-5 h-5 cursor-pointer" /> */}
-            </div>
-
-            {/* Posted Image */}
-
-            {!(post.type === "video") ? (
-              <div className="relative  aspect-square overflow-hidden z-20">
-                {/* If the image src doesn't contaain https:// i.e if its not from firebase or null then show notFoundImg */}
-                <Image
-                  src={
-                    post?.img.includes("https://") ? post.img : notFoundImage
-                  }
-                  alt={post.username}
-                />
-              </div>
-            ) : (
-              <div className="h-auto">
-                <video controls className="h-auto w-full ">
-                  <source src={post.img} type="video/mp4" />
-                </video>
-              </div>
-            )}
-
-            {/* Actions */}
-            <div className="space-y-2">
-              <div className="flex justify-between mb-2">
-                <div className="inline-flex items-center gap-0">
-                  <Button
-                    className="border-none px-[0px]"
-                    onClick={(e) => handleLikes(post)}
-                  >
-                    {post.liked_users &&
-                    post.liked_users.includes(getUser().username) ? (
-                      <HeartFilled className="text-red text-[20px]" />
-                    ) : (
-                      <HeartOutlined className="text-[20px]" />
-                    )}
-                  </Button>
-                  <Button
-                    className="border-none px-[20px]"
-                    onClick={() => {
-                      setPostId(post.id);
-                      showModal();
-                    }}
-                  >
-                    <Badge count={post.totalcomments} showZero>
-                      <MessageOutlined className="text-[20px] text-red-500 !" />
-                    </Badge>
-                  </Button>
-                  <Button className="border-none px-[0px]">
-                    <ShareAltOutlined className="text-[20px] text-red-500 !" />
-                  </Button>
-                </div>
-                <Button className="border-none">
-                  <BookOutlined className="text-[22px]" />
-                </Button>
-              </div>
-              <span className=" font-semibold">{`${post.likes} likes`}</span>
-              <p>
-                <span className="font-semibold">{post.username} </span>
-                {post.caption}
-              </p>
-              <h3 className="text-xs text-gray-500">
-                {moment(post.date).format("LLL")}
-              </h3>
-            </div>
-            <hr className="text-[#d1d5db]" />
-            <div className="flex gap-4" style={{ marginTop: "5px" }}>
-              {post.showEmoji && (
-                <div className="mt-[50px] z-30 absolute !">
-                  <Picker
-                    data={data}
-                    onEmojiSelect={(event: any) => {
-                      handleComment(event, post);
-                    }}
-                    style={{ zIndex: "99" }}
-                    className="z-30 !"
-                  />
+              ) : (
+                <div className="h-auto">
+                  <video controls className="h-auto w-full ">
+                    <source src={post.img} type="video/mp4" />
+                  </video>
                 </div>
               )}
-              <Button className="border-none p-[0px] !">
-                <SmileOutlined
-                  className="text-[18px]"
-                  onClick={() => {
-                    post.showEmoji = !post.showEmoji;
-                    setShowEmojis(!showEmojis);
+
+              {/* Actions */}
+              <div className="space-y-2">
+                <div className="flex justify-between mb-2">
+                  <div className="inline-flex items-center gap-0">
+                    <Button
+                      className="border-none px-[0px]"
+                      onClick={(e) => handleLikes(post)}
+                    >
+                      {post.liked_users &&
+                      post.liked_users.includes(getUser().username) ? (
+                        <HeartFilled className="text-red text-[20px]" />
+                      ) : (
+                        <HeartOutlined className="text-[20px]" />
+                      )}
+                    </Button>
+                    <Button
+                      className="border-none px-[20px]"
+                      onClick={() => {
+                        setPostId(post.id);
+                        showModal();
+                      }}
+                    >
+                      <Badge count={post.totalcomments} showZero>
+                        <MessageOutlined className="text-[20px] text-red-500 !" />
+                      </Badge>
+                    </Button>
+                    <Button className="border-none px-[0px]">
+                      <ShareAltOutlined className="text-[20px] text-red-500 !" />
+                    </Button>
+                  </div>
+                  <Button className="border-none">
+                    <BookOutlined className="text-[22px]" />
+                  </Button>
+                </div>
+                <span className=" font-semibold">{`${post.likes} likes`}</span>
+                <p>
+                  <span className="font-semibold">{post.username} </span>
+                  {post.caption}
+                </p>
+                <h3 className="text-xs text-gray-500">
+                  {moment(post.date).format("LLL")}
+                </h3>
+              </div>
+              <hr className="text-[#d1d5db]" />
+              <div className="flex gap-4" style={{ marginTop: "5px" }}>
+                {post.showEmoji && (
+                  <div className="mt-[50px] z-30 absolute !">
+                    <Picker
+                      data={data}
+                      onEmojiSelect={(event: any) => {
+                        handleComment(event, post);
+                      }}
+                      style={{ zIndex: "99" }}
+                      className="z-30 !"
+                    />
+                  </div>
+                )}
+                <Button className="border-none p-[0px] !">
+                  <SmileOutlined
+                    className="text-[18px]"
+                    onClick={() => {
+                      post.showEmoji = !post.showEmoji;
+                      setShowEmojis(!showEmojis);
+                    }}
+                  />
+                </Button>
+
+                <textarea
+                  className="focus:outline-none w-full resize-none mt-2 pb-0 mb-[-15px]"
+                  key={ind}
+                  value={post.input}
+                  placeholder="Add a comment"
+                  onFocus={() => {
+                    post.showEmoji = false;
+                    setShowEmojis(false);
+                  }}
+                  onKeyDown={(e: any) => {
+                    if (e.key == "Enter") {
+                      addComment(post.id, post);
+                    }
+                  }}
+                  onChange={(event) => {
+                    handleComment(event, post);
                   }}
                 />
-              </Button>
-
-              <textarea
-                className="focus:outline-none w-full resize-none mt-2 pb-0 mb-[-15px]"
-                key={ind}
-                value={post.input}
-                placeholder="Add a comment"
-                onFocus={() => {
-                  post.showEmoji = false;
-                  setShowEmojis(false);
-                }}
-                onKeyDown={(e: any) => {
-                  if (e.key == "Enter") {
-                    addComment(post.id, post);
-                  }
-                }}
-                onChange={(event) => {
-                  handleComment(event, post);
-                }}
-              />
-              <Button
-                onClick={() => addComment(post.id, post)}
-                className="border-none px-[0px] text-[18px] mt-[-2px]"
-              >
-                <SendOutlined />
-              </Button>
+                <Button
+                  onClick={() => addComment(post.id, post)}
+                  className="border-none px-[0px] text-[18px] mt-[-2px]"
+                >
+                  <SendOutlined />
+                </Button>
+              </div>
             </div>
-          </div>
 
-          <hr className="my-[20px] text-[#d1d5db]" />
-        </div>
-      ))}
+            <hr className="my-[20px] text-[#d1d5db]" />
+          </div>
+        ))
+      )}
 
       {isModalOpen ? (
         <Modal
