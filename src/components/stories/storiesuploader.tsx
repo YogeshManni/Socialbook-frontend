3e1566e28762c0d108258ee882d38e6334db907d
Yogesh Manni <yogeshmanni786@gmail.com>
2024-11-05 14:50:39 -0800

    Add story uploader component

---
+++ src/components/stories/storiesuploader.tsx
@@ -0,0 +1,55 @@
+// StoryUploader.jsx
+import { message, Modal, Spin, Upload, UploadProps } from "antd";
+import React, { useState } from "react";
+import upload from "../../lib/upload";
+import { getUser } from "../../helpers/helper";
+import CreatePost from "../createpost/CreatePost";
+
+const StoryUploader = ({ onUpload }: any) => {
+  const [isOpen, setModalOpen] = useState<boolean>(false);
+
+  /* const saveStory = async(url:string) =>
+  {
+    const packet  = 
+    {
+        user_id : ,
+media_url : url,
+media_type : ,
+caption : 
+    }
+  } */
+
+  return (
+    <div className="flex story mr-4">
+      <div className="flex flex-col items-center">
+        <button onClick={() => setModalOpen(true)} className="group relative">
+          <img
+            src={getUser().img}
+            alt={getUser().username}
+            className="object-cover w-16 h-16 rounded-full border-2 border-blue-500 p-0.5 transition-all duration-300 ease-in-out transform group-hover:scale-105"
+          />
+        </button>
+        <span className="mt-1 inline-block leading-tight">+ Upload</span>
+      </div>
+
+      {isOpen && (
+        <Modal
+          title={"Add Story"}
+          open={true}
+          okButtonProps={{ style: { backgroundColor: "#8b5cf6" } }}
+          onCancel={() => setModalOpen(false)}
+          width={800}
+          height={500}
+          footer={null}
+          style={{ top: "10%" }}
+        >
+          <div className="mt-5 md:h-[700px] h-full ">
+            <CreatePost isStory={true} />
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default StoryUploader;
